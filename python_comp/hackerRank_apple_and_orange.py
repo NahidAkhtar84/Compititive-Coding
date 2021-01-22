96d0bc0663b5f48339f41e3dc3ecf61b8005c7db
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-01-22 17:47:16 +0600

    hackerrank apple and orange added Signed-off-by: NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>

---
+++ python_comp/hackerRank_apple_and_orange.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    cnt1=0
+    cnt2=0
+    for i in apples:
+        if a+i >=s and a+i <=t:
+            cnt1 +=1
+        
+    for j in oranges:
+        if b+j >=s and b+j <=t:
+            cnt2 +=1
+    print(cnt1)
+    print(cnt2)
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
